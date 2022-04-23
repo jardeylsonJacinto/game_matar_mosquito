d0cd28175d70aa2334a3ca1ddcba9b5435263d1e
Jardeylson jacinto <jardeylsong.m@gmail.com>
2022-04-23 08:47:20 -0300

    estruturando pg. html e adicionando code js

---
+++ js/jogo.js
@@ -0,0 +1,10 @@
+let altura = 0;
+let largura = 0;
+function ajustaTamanhoPalcoJogo(){
+    altura = window.innerHeight;
+    largura = window.innerWidth;
+
+    console.log(altura,largura);
+}
+    
+ajustaTamanhoPalcoJogo();
